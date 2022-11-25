3b3dbba001040363f5e91505104a9f33b6c4a9ab
yoon ho <dbsgh431@jbnu.ac.kr>
2022-11-25 04:35:44 +0000

    default_auto_aug

--- mmdetection/configs/cv03/_base_/datasets/coco_detection.py
+++ mmdetection/configs/cv03/_base_/datasets/coco_detection.py
@@ -10,6 +10,41 @@ train_pipeline = [
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='AutoAugment',
+         replace = (104, 116, 124)
+        policies=[[
+            dict(
+                type='Resize',
+                img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                           (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                           (736, 1333), (768, 1333), (800, 1333)],
+                multiscale_mode='value',
+                keep_ratio=True)
+        ],
+                    [
+
+
+                      dict(
+                          type='Resize',
+                          img_scale=[(400, 1333), (500, 1333), (600, 1333)],
+                          multiscale_mode='value',
+                          keep_ratio=True),
+                      dict(
+                          type='RandomCrop',
+                          crop_type='absolute_range',
+                          crop_size=(384, 600),
+                          allow_negative_crop=True),
+                      dict(
+                          type='Resize',
+                          img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                                     (576, 1333), (608, 1333), (640, 1333),
+                                     (672, 1333), (704, 1333), (736, 1333),
+                                     (768, 1333), (800, 1333)],
+                          multiscale_mode='value',
+                          override=True,
+                          keep_ratio=True)
+                  ]]),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -31,7 +66,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=8, # gpu 하나 당 올라갈 이미지 수
+    samples_per_gpu=4, # gpu 하나 당 올라갈 이미지 수
     workers_per_gpu=2, # gpu 하나 당 cpu코어 수
     train=dict(
         type=dataset_type,
