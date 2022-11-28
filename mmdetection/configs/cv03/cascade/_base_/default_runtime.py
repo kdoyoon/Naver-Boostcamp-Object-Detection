d62bd2d0296a23e07e52a1ec4411edfe00686b08
jaehyo <chojh0621@gmail.com>
2022-11-28 09:27:31 +0000

    upload

--- mmdetection/configs/cv03/cascade/_base_/default_runtime.py
+++ mmdetection/configs/cv03/cascade/_base_/default_runtime.py
@@ -1,11 +1,6 @@
 import wandb
 
 wandb.login()
-<<<<<<< HEAD:mmdetection/configs/cv03/cascade/_base_/default_runtime.py
-
-=======
-# wandb.init(project="aistages_level2_competition", entity="level2_3")
->>>>>>> b2855873ead6b8dd8a18aa2961c05423c430bfdd:mmdetection/configs/cv03/_base_/default_runtime.py
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -27,11 +22,6 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-<<<<<<< HEAD:mmdetection/configs/cv03/cascade/_base_/default_runtime.py
-
-=======
-#workflow = [('train', 1), ('val', 1)]
->>>>>>> b2855873ead6b8dd8a18aa2961c05423c430bfdd:mmdetection/configs/cv03/_base_/default_runtime.py
 # disable opencv multithreading to avoid system being overloaded
 opencv_num_threads = 0
 # set multi-process start method as `fork` to speed up the training
