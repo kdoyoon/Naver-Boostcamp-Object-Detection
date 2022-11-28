ffec546092a6c1d65fcd19c48f508082787ff93b
yoon ho <dbsgh431@jbnu.ac.kr>
2022-11-28 10:00:03 +0000

    ATSS_SWIN UPLOAD

---
+++ mmdetection/configs/cv03/_base_/atts_swin_dyhead_fpn/_base_/datasets/coco_detection.py
@@ -0,0 +1,57 @@
+import cv2
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root='/opt/ml/dataset/' 
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing") ## class 정의
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(384, 384), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=4, # gpu 하나 당 올라갈 이미지 수
+    workers_per_gpu=2, # gpu 하나 당 cpu코어 수
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'custom/train_2.json',
+        img_prefix=data_root,
+        classes = classes,
+        pipeline=train_pipeline),
+    val=dict(
+    type=dataset_type,
+        ann_file=data_root + 'custom/valid_2.json',
+        img_prefix=data_root,
+        classes = classes,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        classes = classes,
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
---
+++ mmdetection/configs/cv03/_base_/atts_swin_dyhead_fpn/_base_/default_runtime.py
@@ -0,0 +1,40 @@
+import wandb
+
+wandb.login()
+# wandb.init(project="aistages_level2_competition", entity="level2_3")
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='WandbLoggerHook',interval=50,
+            init_kwargs=dict(
+                project='Object_Detection',
+                entity = 'aitech4_cv3',
+                name = "atss_swin_t_fpn_dyhead_20e_coco(YH)"),)
+        # log_checkpoint=True,
+        # log_checkpoint_metadata=True,
+        # num_eval_images=50,
+        # bbox_score_thr=0.1,       
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+#workflow = [('train', 1), ('val', 1)]
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=16)
